04b5307ea149fac60edaeb7d498b0b5dd0c71183
UggeDikshitha3 <dikshithavarma2006@gmail.com>
2026-08-25 19:48:44 +0530

    Fix NameError: import Body in backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
